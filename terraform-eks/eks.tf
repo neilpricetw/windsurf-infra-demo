cf6e2dc8b711ad663196a5535d4c4b08e7fc0a73
Neil Price <neil.price@thoughtworks.com>
2025-07-03 13:59:59 +1000

    Set manage_aws_auth_configmap to false, keep aws_auth_users and aws_auth_roles for documentation or manual use

--- terraform-eks/eks.tf
+++ terraform-eks/eks.tf
@@ -1,5 +1,5 @@
 module "eks" {
-  manage_aws_auth_configmap = true
+  manage_aws_auth_configmap = false
   aws_auth_users = [
     {
       userarn  = "arn:aws:sts::160071257600:assumed-role/AWSReservedSSO_PowerUserPlusRole_db88d920cf78a35f/neil.price@thoughtworks.com"
